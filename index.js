c0b327f938dbf6973bbed9e88c52861e49d7bdc3
adrian411997 <adrian_mart@hotmail.com.ar>
2023-07-06 23:59:58 -0300

    Useroptions

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const routes = require("./src/Routes/index.js");
 const app = express();
 const PORT = 3001;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "false");
   res.header(
     "Access-Control-Allow-Headers",
@@ -21,9 +20,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-server.use((err, req, res, next) => {
+app.use("/api", routes);
+app.use((err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.mesaage || err;
+  const message = err.message || err;
   console.log(err);
   res.status(status).send(message);
 });
@@ -31,5 +31,3 @@ server.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
-
-module.exports = server;
--- src/controllers/CRUD.js
+++ src/controllers/CRUD.js
@@ -1,9 +1,10 @@
 const db = require("../db");
 
+//USUARIOS
 const postUsuario = async (req, res, next) => {
   try {
-    let { nombre, email, password } = req.body;
-
+    const { nombre, email, password } = req.body;
+    console.log(nombre, email, password);
     db.all(
       "SELECT * FROM usuario WHERE nombre = ? OR email = ?",
       [nombre, email],
@@ -39,4 +40,62 @@ const postUsuario = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = postUsuario;
+const getAllUsuario = async (req, res, next) => {
+  db.all("SELECT * FROM usuario", [], function (err, rows) {
+    if (err) {
+      throw err;
+    }
+
+    let users = rows;
+    console.log(users);
+    return res.status(200).json({ users });
+  });
+};
+const updateUsuarioNombre = async (req, res, next) => {
+  let { id } = req.query;
+  let { nombre } = req.body;
+  console.log(id, nombre);
+  try {
+    db.run(
+      "UPDATE usuario SET nombre = ? WHERE id = ?",
+      [nombre, id],
+      function (err) {
+        if (err) {
+          throw err;
+        } else {
+          res.status(200).json({ message: "Nombre cambiado con éxito" });
+        }
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+const updateUsuarioPassword = async (req, res, next) => {
+  let { id } = req.query;
+  let { password } = req.body;
+  db.run(
+    "UPDATE usuario SET password = ? WHERE id = ?",
+    [password, id],
+    function (err) {
+      if (err) {
+        throw err;
+      } else {
+        res.status(400).json({ message: "Contraseña cambiado con éxito" });
+      }
+    }
+  );
+};
+const deleteAlluser = async (req, res, next) => {
+  db.run("DELETE FROM usuario", [], function (err) {
+    if (err) throw err;
+  });
+  res.json({ message: "Eliminados todos papu" });
+};
+module.exports = {
+  postUsuario,
+  getAllUsuario,
+  updateUsuarioNombre,
+  updateUsuarioPassword,
+  deleteAlluser,
+};
